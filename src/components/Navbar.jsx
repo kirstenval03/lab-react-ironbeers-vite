5498edeb2150e23f6e026a30c5cbbf37413a2662
kirstenval03 <kirs0307@gmail.com>
2023-07-28 15:03:07 -0600

    Solved lab :)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,16 @@
-function Navbar() {}
+import React from 'react'
+import homeIcon from "../assets/home-icon.png";
+import { Link } from "react-router-dom";
 
-export default Navbar;
+function Navbar() {
+    return (
+        <nav>
+            <Link to={"/"}>
+                <img src={homeIcon} alt="" />
+            </Link>
+        </nav>
+
+    )
+}
+
+export default Navbar
